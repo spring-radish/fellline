e669a100d9b92aabcc26b8fdb56a53654e992fe2
spring <mail@sprig.site>
2024-11-03 00:20:43 -0500

    in-place bundling

--- build.js
+++ build.js
@@ -6,6 +6,6 @@ const data = readFileSync(pwa_path, 'utf8')
 const now = Date.now()
 const stampedData = data.replace("{{BUILD_VERSION}}", now)
 
-writeFileSync('./src/scripts/pwa-out.js', stampedData)
+writeFileSync(pwa_path, stampedData)
 
 console.log('wrote file')
